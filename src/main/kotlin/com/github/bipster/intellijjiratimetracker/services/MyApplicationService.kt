55f576db2c4e8cbc2be24a6bbd3f22c639fd625d
GitHub Action <action@github.com>
2020-09-26 00:53:19 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/bipster/intellijjiratimetracker/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.bipster.intellijjiratimetracker.services
+
+import com.github.bipster.intellijjiratimetracker.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/bipster/intellijjiratimetracker/services/MyProjectService.kt
+++ src/main/kotlin/com/github/bipster/intellijjiratimetracker/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.bipster.intellijjiratimetracker.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bipster.intellijjiratimetracker.MyBundle
 
 class MyProjectService(project: Project) {
 
